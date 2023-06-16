b19f4416ec20afab6e5f21a00cefe64b9a409086
Pierre21200 <pierrepotin21@gmail.com>
2023-06-16 12:24:15 +0200

    var

--- src/utils/fetch/index.jsx
+++ src/utils/fetch/index.jsx
@@ -1,6 +1,8 @@
 import emailjs from 'emailjs-com';
+const dotenv = require("dotenv");
+dotenv.config();
 
-const BACKEND_URL = 'https://bioback.herokuapp.com'
+const BACKEND_URL = process.env.BACKEND_URL;
 
 function convertDate(dateString) {
   const daysOfWeek = [
